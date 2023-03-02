8c3aad91bfe9565eafeda8f12b3f112fedba775b
qvanle <vanle199tk@gmail.com>
2023-03-02 16:48:42 +0700

    change insert mode to normal mode when open file explorer

--- lua/mapping/nvim-tree.lua
+++ lua/mapping/nvim-tree.lua
@@ -1,5 +1,5 @@
 -- file explorer
 vim.keymap.set('n', '<C-f>', ":NvimTreeFindFileToggle<CR>", {silent = true})
-vim.keymap.set('i', '<C-f>', "<c-o>:NvimTreeFindFileToggle<CR>", {silent = true})
+vim.keymap.set('i', '<C-f>', "<esc>:NvimTreeFindFileToggle<CR>", {silent = true})
 
 
