96e7d0102ebbaff9331c8f019fc9f8a8b2d21b09
qvanle <vanle199tk@gmail.com>
2024-05-17 14:17:24 +0700

    move old mapping to subfolder qwerty

---
+++ lua/mapping/qwerty/init.lua
@@ -0,0 +1,7 @@
+file = require("src.file")
+
+file.sourceFile("mapping/qwerty/basic")
+file.sourceFile("mapping/qwerty/nvim-tree")
+file.sourceFile("mapping/qwerty/telescope")
+file.sourceFile("mapping/qwerty/floaterm")
+file.sourceFile("mapping/qwerty/copilot")
