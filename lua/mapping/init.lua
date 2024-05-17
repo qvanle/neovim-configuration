3981582889479126c7bc66ca2c7c6518b8103211
qvanle <vanle199tk@gmail.com>
2024-05-17 14:17:53 +0700

    add dvorak layout

--- lua/mapping/init.lua
+++ lua/mapping/init.lua
@@ -1,7 +1,3 @@
 file = require("src.file")
 
-file.sourceFile("mapping/basic")
-file.sourceFile("mapping/nvim-tree")
-file.sourceFile("mapping/telescope")
-file.sourceFile("mapping/floaterm")
-file.sourceFile("mapping/copilot")
+file.sourceFile("mapping/dvorak/init")
