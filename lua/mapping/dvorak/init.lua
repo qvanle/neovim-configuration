c6bbf1a864453665980dec36516ae5345b5251ea
qvanle <vanle199tk@gmail.com>
2024-05-17 14:19:47 +0700

    add dvorak layout

---
+++ lua/mapping/dvorak/init.lua
@@ -0,0 +1,11 @@
+file = require("src.file")
+
+file.sourceFile("mapping/dvorak/unmap-default")
+file.sourceFile("mapping/dvorak/mode")
+file.sourceFile("mapping/dvorak/moving")
+file.sourceFile("mapping/dvorak/edit")
+
+-- plugin
+file.sourceFile("mapping/dvorak/nvim-tree")
+file.sourceFile("mapping/dvorak/copilot")
+
