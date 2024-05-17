68bc51dbc7b331248d892430f06ded549c4f15a1
qvanle <vanle199tk@gmail.com>
2024-05-17 14:19:12 +0700

    add the return value of source lua file

--- lua/src/file.lua
+++ lua/src/file.lua
@@ -1,16 +1,17 @@
 local M = {}
 
 function M.sourceFile(dir) 
-    fullpath = vim.g.CONFIG_PATH .. "lua/" .. dir .. ".lua" 
+    fullpath = vim.g.CONFIG_PATH .. "lua/" .. dir .. ".lua"
     if(vim.fn.filereadable(fullpath) == 1) then
-        require(dir)
+        return require(dir)
     else 
         print("File not found: " .. fullpath)
+        return nil
     end
 end
 
 function M.sourceFolder(dir)
-    M.sourceFile(dir .. "/init")
+    return M.sourceFile(dir .. "/init")
 end
 
 return M
