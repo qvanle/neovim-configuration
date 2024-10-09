5eda186fd43bef8cacae3576971130fdd2e82e22
qvanle <vanle199tk@gmail.com>
2024-10-09 15:22:42 +0700

    jump to error

--- lua/mapping/dvorak/lsp.lua
+++ lua/mapping/dvorak/lsp.lua
@@ -6,6 +6,7 @@ vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { n
 vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('n', 'ee', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'EE', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'ff', '<cmd>lua vim.lsp.buf.format({async = true})<CR>', { noremap = true, silent = true })
 
